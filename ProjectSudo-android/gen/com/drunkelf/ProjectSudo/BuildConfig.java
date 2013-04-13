064a98e52d1bf8b0daffeb526058f9c5d10eb850
Todd Gibson <todd@ballistiksoftware.com>
2013-04-12 21:45:47 -0400

    initial commit

---
+++ ProjectSudo-android/gen/com/drunkelf/ProjectSudo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.drunkelf.ProjectSudo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ ProjectSudo-desktop/src/com/drunkelf/ProjectSudo/Main.java
@@ -0,0 +1,16 @@
+package com.drunkelf.ProjectSudo;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "ProjectSudo";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new ProjectSudo(), cfg);
+	}
+}
